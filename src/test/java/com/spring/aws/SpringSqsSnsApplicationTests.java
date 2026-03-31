911302f8b0d9222d6eeabd22de20c74360671553
man nguyen <minhman2.nguyen@mafc.com.vn>
2026-03-31 10:26:43 +0700

    init project

---
+++ src/test/java/com/spring/aws/SpringSqsSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSqsSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
